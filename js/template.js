5ca4856745deba183f01d07a9311c01c98d5b617
Micaiah Cape <mchlcape808@gmail.com>
2021-10-17 18:48:27 -1000

    finished substance page

--- js/template.js
+++ js/template.js
@@ -8,22 +8,6 @@ function hideMobile(){
     document.getElementById('emergencyMobileHeader').style.display = 'none';
 }
 
-function showMoreStats(){
-    document.getElementById('entireStatHolder2').style.display = 'block';
-    document.getElementById('entireStatHolder2').style.display = 'flex';
-    document.getElementById('entireStatHolder2').scrollIntoView();
-    document.getElementById('seeMoreStats').innerHTML = ""
-    this.removeEventListener('click', showMoreStats);
-
-    var link = document.createElement('a');
-    link.id = 'addedLink';
-    var insideLink = document.createTextNode('Want to see even more stats? Check out the Statistics page.');
-    link.setAttribute('href', '/stats');
-    link.setAttribute('target', '_blank');
-    link.appendChild(insideLink);
-    document.getElementById('seeMoreStats').appendChild(link);
-}
-
 function showMenu(){
     a++;
     if(a%2 === 1){
@@ -41,6 +25,5 @@ function scrollBackToTop(){
 
 document.getElementById('close').addEventListener('click', hide);
 document.getElementById('closeMobile').addEventListener('click', hideMobile);
-document.getElementById('seeMoreStats').addEventListener('click', showMoreStats);
 document.getElementById('menu').addEventListener('click', showMenu);
 document.getElementById('backToTop').addEventListener('click', scrollBackToTop);
